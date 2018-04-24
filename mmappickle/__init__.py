87071deba6f967f54b820a06b1ab10603e678e4d
Laurent Fasnacht <l@libres.ch>
2018-04-24 08:37:12 +0200

    Remove httpdict from master, as it is not production ready

--- mmappickle/__init__.py
+++ mmappickle/__init__.py
@@ -1,11 +1,2 @@
 from .dict import mmapdict
 __all__ = ['mmapdict']
-
-try:
-    from .http import httpdict
-    __all__.append('httpdict')
-except ImportError:
-    #Ignore missing dependencies, like requests
-    pass
-
-
